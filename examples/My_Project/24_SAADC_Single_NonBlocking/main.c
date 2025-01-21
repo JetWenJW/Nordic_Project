c795c6c57941bffd7c5a3f22cf19f8ffe08859a5
JetWen <killer500k@gmail.com>
2025-01-21 22:53:36 +0800

    [Nordic BLE Project] 1. Modified the Project which is named as 24_SAADC_Single_NonBlocking 2. Add the PPI channel to SAADC

--- examples/My_Project/24_SAADC_Single_NonBlocking/main.c
+++ examples/My_Project/24_SAADC_Single_NonBlocking/main.c
@@ -26,6 +26,48 @@
 #define SAMPLE_BUFFER_LEN 5
 static nrf_saadc_value_t m_buffer_pool[2][SAMPLE_BUFFER_LEN];
 
+/* Declait Timer & PPI Channel */
+static const nrf_drv_timer_t m_timer = NRF_DRV_TIMER_INSTANCE(1);
+static nrf_ppi_channel_t m_ppi_channel;
+
+void timer_handler(nrf_timer_event_t event_type, void * p_context)
+{
+
+
+}
+
+void timer_with_ppi_init(void)
+{
+    ret_code_t err_code;
+
+    err_code = nrf_drv_ppi_init();
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
+    timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_32;
+
+    err_code = nrf_drv_timer_init(&m_timer, &timer_cfg, timer_handler);
+    APP_ERROR_CHECK(err_code);
+
+    uint32_t ticks = nrf_drv_timer_ms_to_ticks(&m_timer, 50);
+
+    nrf_drv_timer_extended_compare(&m_timer, NRF_TIMER_CC_CHANNEL0, ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, false);
+    nrf_drv_timer_enable(&m_timer);
+
+    uint32_t timer_event_compare_addr = nrf_drv_timer_compare_event_address_get(&m_timer, NRF_TIMER_CC_CHANNEL0);
+    uint32_t saadc_sample_task_addr = nrf_drv_saadc_sample_task_get();
+
+    err_code = nrf_drv_ppi_channel_alloc(&m_ppi_channel);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_drv_ppi_channel_assign(m_ppi_channel, timer_event_compare_addr, saadc_sample_task_addr);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+
+
+/* SAADC Call Back Handler Function */
 void saadc_callback_handler(nrf_drv_saadc_evt_t const * p_evt)
 {
     float value;
@@ -77,13 +119,18 @@ int main(void)
     NRF_LOG_DEFAULT_BACKENDS_INIT();
 
     saadc_init();
+
+    timer_with_ppi_init();
+
+    ret_code_t err_code = nrf_drv_ppi_channel_enable(m_ppi_channel);
+    APP_ERROR_CHECK(err_code);
+
     NRF_LOG_INFO("SAADC Application Start...");
 
     while (1)
     {
         NRF_LOG_PROCESS();
-        nrfx_saadc_sample();
-        nrf_delay_ms(1000);
+
     }
 }
 
